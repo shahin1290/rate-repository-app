55e8b777a9f66219aafb9da8805ce80d7ec7f0c2
shahin1290 <s.patowary@yahoo.com>
2021-01-24 20:14:45 +0100

    finishes Exercise 10.3: the reviewed repositories list

---
+++ src/components/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Constants from 'expo-constants';
+import { Text, StyleSheet, View } from 'react-native';
+import RepositoryList from './RepositoryList';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+  header: {
+    marginBottom: 10,
+     fontSize: 15,
+    fontWeight: 'bold'
+  }
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Rate Repository Application</Text>
+      <RepositoryList />
+    </View>
+  );
+};
+
+export default Main;
