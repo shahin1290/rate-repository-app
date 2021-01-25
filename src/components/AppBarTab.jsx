3f2a4dfb28ad270cf5f68bb9331cbb00ea44d54e
shahin1290 <s.patowary@yahoo.com>
2021-01-25 21:42:22 +0100

    finishes Exercise 10.6: the sign in view

--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import { StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Link } from 'react-router-native';
+
 import Text from './Text';
 
 const styles = StyleSheet.create({
   innerText: {
     color: 'white',
   },
+  flexContainer: {
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
 });
 
 const Main = () => {
   return (
-    <TouchableWithoutFeedback>
-      <Text style={styles.innerText} fontWeight='bold'>
-        Repositories
-      </Text>
-    </TouchableWithoutFeedback>
+    <View style={styles.flexContainer}>
+      <Link to='/' component={TouchableOpacity} activeOpacity={0.8}>
+        <Text style={styles.innerText} fontWeight='bold'>
+          Repositories
+        </Text>
+      </Link>
+
+      <Link to='/sign-in' component={TouchableOpacity} activeOpacity={0.8}>
+        <Text style={styles.innerText} fontWeight='bold'>
+          Sign in
+        </Text>
+      </Link>
+    </View>
   );
 };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Route, Switch, Redirect } from 'react-router-native';
+
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import theme from '../theme';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: theme.colors.mainBackground,
     flexGrow: 1,
     flexShrink: 1,
   },
@@ -14,7 +19,15 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Switch>
+        <Route path="/" exact>
+          <RepositoryList />
+        </Route>
+        <Route path="/sign-in" exact>
+          <SignIn />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </View>
   );
 };
