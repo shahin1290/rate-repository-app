c04a62d7be583708bb88f001cc30f2fff58f7887
shahin1290 <s.patowary@yahoo.com>
2021-01-26 19:02:05 +0100

    Exercise 10.10: a platform specific font

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,23 @@
+import { Platform } from 'react-native';
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
     textSecondary: '#fff',
     primary: '#0366d6',
     barBackground: '#24292e',
-    danger: '#d73a4a'
+    danger: '#d73a4a',
   },
   fontSizes: {
     body: 14,
     subheading: 16,
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      ios: 'Arial',
+      android: 'Roboto',
+      default: 'system',
+    }),
   },
   fontWeights: {
     normal: '400',
